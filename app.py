2c688c4a3b6f149de967efb0c7721ead34c8323a
georg0n <geo@mailbo.ru>
2023-05-11 06:49:16 +0300

    finally

--- app.py
+++ app.py
@@ -1,16 +1,52 @@
-from flask import Flask, render_template, url_for
-from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import openpyxl as op
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        title = request.form.get('title')
+        return redirect(url_for('start', title=title))
+    return render_template('index.html')
+
+
+@app.route('/start')
+def start():
+    title = request.args.get('title', None)
+    filename = 'IIT_1-kurs_22_23_vesna_TANDEM_29.03.2023.xlsx'
+
+    wb = op.load_workbook(filename, data_only=True)
+
+    sheet = wb.active
+    max_column = sheet.max_column
+    arr = []
+    group = title
+    for i in range(2, max_column+1):
+        search_group = sheet.cell(row=2, column=i).value
+        numberOfColumn = i
+        if not search_group:
+            continue
+
+        if (search_group == group):
+            for j in range(4, 88):
+                arr.append(sheet.cell(row=j, column=i).value)
+
+
+    return render_template('start.html', title=title, arr=arr)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
 
 
 
